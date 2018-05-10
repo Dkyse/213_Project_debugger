1a18b08d0304ac0f59f3630df80c58d8af955d7e
David Neill Asanza <davidhneill@gmail.com>
2018-05-09 19:01:08 -0500

    Made a shared object class to cleanup main file.

--- experiment_1_stop_by_instructions/experiment.cpp
+++ experiment_1_stop_by_instructions/experiment.cpp
@@ -27,184 +27,18 @@
 #include "elf++.hh"
 #include "dwarf++.hh"
 #include "breakpoint.hh"
+#include "shared_object.hh"
 
 using dwarf::compilation_unit;
 using std::vector;
 using std::string;
-
-typedef struct shared_obj {
-  intptr_t addr_start;  // Start address of object
-  intptr_t addr_end;    // End address of object
-  string name;          // Object name
-  elf::et type;         // Object file type
-  bool has_cus;         // Whether object has associated compilation units
-  vector<compilation_unit> compilation_units; // Object's compilation units
-} shared_obj_t;
-
-/***************************
-* BEGIN TESTING FUNCTIONS *
-***************************/
-void
-dump_line_table(const dwarf::line_table &lt)
-{
-  for (auto &line : lt) {
-    if (line.end_sequence)
-    printf("\n");
-    else
-    printf("%-40s%8d%#20" PRIx64 "\n", line.file->path.c_str(),
-    line.line, line.address);
-  }
-}
-
-void
-dump_all_line_tables(const std::vector<compilation_unit> &cus) {
-  for (auto cu : cus) {
-    printf("--- <%x>\n", (unsigned int)cu.get_section_offset());
-    dump_line_table(cu.get_line_table());
-    printf("\n");
-  }
-}
-/*************************
-* END TESTING FUNCTIONS *
-*************************/
-// TODO these names are terrible! change them!
-intptr_t relative_ip_offset(shared_obj_t &obj, intptr_t ip) {
-  switch (obj.type) {
-    case elf::et::exec:
-    // exec files have relative addresses
-    return ip;
-    break;
-
-    case elf::et::dyn:
-    // dynamic files have absolute addresses
-    return ip - obj.addr_start;
-    break;
-
-    default:
-    // We don't consider other types
-    return ip;
-  }
-}
-
-/**
-*
-* @param  obj [description]
-* @param  ip  instruction pointer address relative to object start address
-* @return     [description]
-*/
-// TODO these names are terrible! change them!
-intptr_t absolute_ip_offset(shared_obj_t &obj, intptr_t ip) {
-  switch (obj.type) {
-    case elf::et::exec:
-    // exec files have relative addresses
-    return ip;
-    break;
-
-    case elf::et::dyn:
-    // dynamic files have absolute addresses
-    return ip + obj.addr_start;
-    break;
-
-    default:
-    // We don't consider other types
-    return ip;
-  }
-}
-
-/**
-* [get_line_entry_from_ip description]
-* @param  pc [description]
-* @return    [description]
-* Source:
-* https://blog.tartanllama.xyz/writing-a-linux-debugger-source-signal/
-*/
-dwarf::line_table::iterator get_line_entry_from_ip(const std::vector<compilation_unit> &cus, intptr_t ip) {
-  /* walk through the each compilation unit's line table to find the line */
-  for (auto &cu : cus) {
-    if (die_pc_range(cu.root()).contains(ip)) {
-      auto &lt = cu.get_line_table();
-      auto it = lt.find_address(ip);
-      if (it == lt.end()) {
-        throw std::out_of_range{"Cannot find line entry"};
-      }
-      else {
-        return it;
-      }
-    }
-  }
-
-  throw std::out_of_range{"Cannot find line entry"};
-}
-
-/**
-* [get_line_entry_from_ip description]
-* @param  pc [description]
-* @return    [description]
-* Source:
-* https://blog.tartanllama.xyz/writing-a-linux-debugger-source-signal/
-*/
-dwarf::line_table::iterator get_line_entry_from_function(const std::vector<compilation_unit> &compilation_units, const std::string& name) {
-  /* walk through the each compilation unit's line table to find the line */
-  for (const auto& cu : compilation_units) {
-    for (const auto& die : cu.root()) {
-      if (die.has(dwarf::DW_AT::name) && at_name(die) == name) {
-        // Find lowest address for this function DIE
-        auto low_ip = at_low_pc(die);
-        auto &lt = cu.get_line_table();
-        auto entry = lt.find_address(low_ip);
-        if (entry == lt.end()) {
-          throw std::out_of_range{"Cannot find line entry"};
-        }
-        else {
-          // skip function prologue to point to actual user code
-          return ++entry;
-        }
-      }
-    }
-  }
-  throw std::out_of_range{"Cannot find line entry"};
-}
-
-/**
-* Given an instruction pointer and its object, find line info
-* @param  obj a shared object entry
-* @param  rip instruction pointer
-* @return     true if the line is found, false otherwise
-*/
-bool print_line_info(shared_obj_t &obj, intptr_t rip) {
-  /* calculate offset of the instruction pointer from the beginning of the file */
-  intptr_t file_off = relative_ip_offset(obj, rip);
-
-  /* return value set to false */
-  bool found = false;
-
-  /* a line table */
-  dwarf::line_table lt;
-
-  /* if the object has line table */
-  if (obj.has_cus)  {
-    try {
-      auto entry = get_line_entry_from_ip(obj.compilation_units, file_off);
-      /* If we find the line, print it */
-      printf("File path: %s\n", entry->file->path.c_str());
-      printf("Called from line %u\n\n", entry->line);
-      found = true;
-    } catch(std::out_of_range &e) {
-      /* Line was not found */
-      printf("File path: %s\n", obj.name.c_str());
-      printf("No line numbers found.\n\n");
-    }
-  }
-  return found;
-}
-
 
 /**
 * [populate_shared_objs description]
 * Source:
 * https://stackoverflow.com/questions/36523584/how-to-see-memory-layout-of-my-program-in-c-during-run-time/36524010
 */
-int populate_shared_objs(pid_t child, vector<shared_obj_t> &objects) {
+int populate_shared_objs(pid_t child, vector<shared_obj> &objects) {
   char* line = NULL;
   size_t size = 0;
   char maps_path[128];
@@ -217,8 +51,6 @@ int populate_shared_objs(pid_t child, vector<shared_obj_t> &objects) {
 
   // Parse maps file
   while (getline(&line, &size, maps) > 0) {
-
-    shared_obj_t obj;
 
     // Temporary variables to store parsed data
     char           perms[8];
@@ -246,26 +78,13 @@ int populate_shared_objs(pid_t child, vector<shared_obj_t> &objects) {
 
       /* Check if entry is a readable file */
       if( fd != -1 ) {
-        // file exists
-        obj.name = name;
-
-        obj.addr_start = addr_start;
-        obj.addr_end = addr_end;
-
-        // Get compilation units for this file
-        try {
-          elf::elf elf(elf::create_mmap_loader(fd));
-          dwarf::dwarf dwarf(dwarf::elf::create_loader(elf));
-          obj.type = elf.get_hdr().type;
-          obj.compilation_units = dwarf.compilation_units();
-          obj.has_cus = true;
-        } catch(dwarf::format_error& e) {
-          obj.has_cus = false;
-        }
+        // file exists, create shared object
+        shared_obj obj (name, fd, addr_start, addr_end);
 
         // Add object to vector
         objects.push_back(obj);
       }
+      close(fd);
     }
   } /* end of while */
 
@@ -280,7 +99,7 @@ int populate_shared_objs(pid_t child, vector<shared_obj_t> &objects) {
 * @param child    [description]
 * @param main_obj [description]
 */
-void break_at_main(pid_t child, shared_obj_t &main_obj) {
+void break_at_main(pid_t child, shared_obj &main_obj) {
   // To store waitpid status
   int status;
   // To store debuggee status
@@ -288,13 +107,14 @@ void break_at_main(pid_t child, shared_obj_t &main_obj) {
 
   try {
     // Get main function line table entry
-    auto main_entry = get_line_entry_from_function(main_obj.compilation_units, "main");
+    auto main_entry = main_obj.get_line_entry_from_function("main");
+    // TODO remove
     printf("MAIN: %lx\n\n", main_entry->address);
-    dump_all_line_tables(main_obj.compilation_units);
+    main_obj.dump_all_line_tables();
     getchar();
     printf("\n\n");
     // Set breakpoint
-    breakpoint bp {child, absolute_ip_offset(main_obj, main_entry->address)};
+    breakpoint bp {child, main_obj.absolute_ip_offset(main_entry->address)};
     bp.enable();
     // Continue until we reach the breakpoint
     intptr_t prev_ip;
@@ -313,8 +133,38 @@ void break_at_main(pid_t child, shared_obj_t &main_obj) {
     // restore bp instruction
     bp.disable();
   } catch(std::out_of_range &e) {
-    fprintf(stderr, "'%s' main function not found", main_obj.name.c_str());
+    fprintf(stderr, "'%s' main function not found", main_obj.get_name().c_str());
   }
+}
+
+/**
+* Given an instruction pointer and its object, find line info
+* @param  obj a shared object entry
+* @param  rip instruction pointer
+* @return     true if the line is found, false otherwise
+*/
+bool print_line_info(shared_obj &obj, intptr_t rip) {
+  /* return value set to false */
+  bool found = false;
+
+  /* if the object has line table */
+  if (obj.has_cus())  {
+    try {
+      auto entry = obj.get_line_entry_from_ip(rip);
+      /* If we find the line, print it */
+      printf("File path: %s\n", entry->file->path.c_str());
+      printf("Called from line %u\n\n", entry->line);
+      found = true;
+    } catch(std::out_of_range &e) {
+      /* Line was not found */
+      printf("File path: %s\n", obj.get_name().c_str());
+      printf("No line numbers found.\n\n");
+    }
+  } else {
+    printf("File path: %s\n", obj.get_name().c_str());
+    printf("No debug information available.\n\n");
+  }
+  return found;
 }
 
 int main(int argc, char** argv)  {
@@ -336,7 +186,7 @@ int main(int argc, char** argv)  {
   inputs[argc-1] = NULL;
 
   /* a vector to store information and line-table for all files involved */
-  vector<shared_obj_t> shared_objs;
+  vector<shared_obj> shared_objs;
 
   /* debuggee */
   pid_t child;
@@ -374,7 +224,7 @@ int main(int argc, char** argv)  {
     // TODO remove
     printf("SHARED OBJECT TABLE:\n");
     for (auto obj : shared_objs) {
-      printf("%lx-%lx\t%hu %s\n", obj.addr_start, obj.addr_end, obj.type, obj.name.c_str());
+      obj.print_string_form();
     }
     getchar();
     printf("\n\n");
@@ -401,16 +251,16 @@ int main(int argc, char** argv)  {
       ptrace(PTRACE_GETREGS, current, NULL, &regs);
       /* We don't know how to recover from this if ptrace fails. So we break */
 
-      /* for each instruction call, check which lib did it come from
+      /* for each instruction call, determine which source file it comes from
       * by walking through the shared_obj table
       */
       for (auto obj : shared_objs) {
         /* if a file is found, check line table for that instruction */
-        if (obj.addr_start <= regs.rip && regs.rip <= obj.addr_end) {
+        if (obj.contains(regs.rip)) {
           printf("Thread ID (PID): %d | Instruction address: %llx\n", current, regs.rip);
           bool found = print_line_info(obj, regs.rip);
           if (found) {
-            // Stop execution when line number is found
+            // Stop execution when next line number is found
             getchar();
           }
           break;
---
+++ experiment_1_stop_by_instructions/shared_object.cpp
@@ -0,0 +1,160 @@
+#include <inttypes.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <sys/ptrace.h>
+
+#include "shared_object.hh"
+
+shared_obj::shared_obj(std::string file_path, int file_descriptor, intptr_t addr_start, intptr_t addr_end) {
+  // file exists
+  this->name = file_path;
+
+  this->addr_start = addr_start;
+  this->addr_end = addr_end;
+
+  // Get compilation units for this file
+  try {
+    elf::elf elf(elf::create_mmap_loader(file_descriptor));
+    dwarf::dwarf dwarf(dwarf::elf::create_loader(elf));
+    this->type = elf.get_hdr().type;
+    this->compilation_units = dwarf.compilation_units();
+    this->has_compilation_units = true;
+  } catch(dwarf::format_error& e) {
+    this->has_compilation_units = false;
+  }
+}
+
+/**
+* [get_line_entry_from_ip description]
+* @param  pc [description]
+* @return    [description]
+* Source:
+* https://blog.tartanllama.xyz/writing-a-linux-debugger-source-signal/
+*/
+dwarf::line_table::iterator shared_obj::get_line_entry_from_ip(intptr_t ip) {
+  /* calculate offset of the instruction pointer from the beginning of the file */
+  intptr_t file_off = relative_ip_offset(ip);
+
+  /* walk through the each compilation unit's line table to find the line */
+  for (auto &cu : compilation_units) {
+    if (die_pc_range(cu.root()).contains(file_off)) {
+      auto &lt = cu.get_line_table();
+      auto it = lt.find_address(file_off);
+      if (it == lt.end()) {
+        throw std::out_of_range{"Cannot find line entry"};
+      }
+      else {
+        return it;
+      }
+    }
+  }
+
+  throw std::out_of_range{"Cannot find line entry"};
+}
+
+/**
+* [shared_obj::get_line_entry_from_ip description]
+* @param  pc [description]
+* @return    [description]
+* Source:
+* https://blog.tartanllama.xyz/writing-a-linux-debugger-source-signal/
+*/
+dwarf::line_table::iterator shared_obj::get_line_entry_from_function(const std::string& name) {
+  /* walk through the each compilation unit's line table to find the line */
+  for (const auto& cu : compilation_units) {
+    for (const auto& die : cu.root()) {
+      if (die.has(dwarf::DW_AT::name) && at_name(die) == name) {
+        // Find lowest address for this function DIE
+        auto low_ip = at_low_pc(die);
+        auto &lt = cu.get_line_table();
+        auto entry = lt.find_address(low_ip);
+        if (entry == lt.end()) {
+          throw std::out_of_range{"Cannot find line entry"};
+        }
+        else {
+          // skip function prologue to point to actual user code
+          return ++entry;
+        }
+      }
+    }
+  }
+  throw std::out_of_range{"Cannot find line entry"};
+}
+
+// TODO these names are terrible! change them!
+intptr_t shared_obj::relative_ip_offset(intptr_t ip) {
+  switch (type) {
+    case elf::et::exec:
+    // exec files have relative addresses
+    return ip;
+    break;
+
+    case elf::et::dyn:
+    // dynamic files have absolute addresses
+    return ip - addr_start;
+    break;
+
+    default:
+    // We don't consider other types
+    return ip;
+  }
+}
+
+/**
+*
+* @param  obj [description]
+* @param  ip  instruction pointer address relative to object start address
+* @return     [description]
+*/
+// TODO these names are terrible! change them!
+intptr_t shared_obj::absolute_ip_offset(intptr_t ip) {
+  switch (type) {
+    case elf::et::exec:
+    // exec files have relative addresses
+    return ip;
+    break;
+
+    case elf::et::dyn:
+    // dynamic files have absolute addresses
+    return ip + addr_start;
+    break;
+
+    default:
+    // We don't consider other types
+    return ip;
+  }
+}
+
+bool shared_obj::contains(intptr_t ip) {
+  return (addr_start <= ip) && (ip <= addr_end);
+}
+
+/***************************
+* BEGIN TESTING FUNCTIONS *
+***************************/
+void dump_line_table(const dwarf::line_table &lt)
+{
+  for (auto &line : lt) {
+    if (line.end_sequence)
+    printf("\n");
+    else
+    printf("%-40s%8d%#20" PRIx64 "\n", line.file->path.c_str(),
+    line.line, line.address);
+  }
+}
+
+void shared_obj::dump_all_line_tables() {
+  for (auto cu : compilation_units) {
+    printf("--- <%x>\n", (unsigned int)cu.get_section_offset());
+    dump_line_table(cu.get_line_table());
+    printf("\n");
+  }
+}
+
+void shared_obj::print_string_form() {
+  printf("%lx-%lx\t%hu %s\n", addr_start, addr_end, type, name.c_str());
+}
+/*************************
+* END TESTING FUNCTIONS *
+*************************/
